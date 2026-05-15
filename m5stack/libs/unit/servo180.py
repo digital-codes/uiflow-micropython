7999a7dd8bbafd821c4b825f3fd2c1883a7f5870
luoweiyuan <luoweiyuan@m5stack.com>
2026-05-15 13:38:05 +0800

    unit: Fix servo180/360 driver error.
    Signed-off-by: luoweiyuan <luoweiyuan@m5stack.com>

--- m5stack/libs/unit/servo180.py
+++ m5stack/libs/unit/servo180.py
@@ -44,7 +44,8 @@ class Servo180Unit:
         self._count_low = count_low
         self._count_high = count_high
         self._last_duty = self._count_low
-        self.pwm = machine.PWM(pin, freq=freq, duty_ns=self._count_low * 1000)
+        self.pwm = machine.PWM(pin, freq=freq)
+        self.pwm.duty_ns(self._count_low * 1000)
 
     def set_angle(self, angle: int, wait=True) -> None:
         """Set the servo to a specific angle.
